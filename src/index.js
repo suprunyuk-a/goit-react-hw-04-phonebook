d04aa71fdeb1b4bea88848a0badb7f2329e96053
suprunyuk-a <chaserenergy@gmail.com>
2022-11-14 20:52:58 +0200

    add work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { default as React } from 'react';
+import { default as ReactDOM } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import './index.css';
+import { App } from './components/App';
+import { theme } from './constants';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
